90d2ad21eeaee62e3fa73d3e6f90c2d911c23e73
milaha <146625448+Mrkdsjjsjs@users.noreply.github.com>
2025-04-12 16:21:36 +0300

    fix

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,13 +1,21 @@
-from datetime import datetime, timedelta
-from jose import jwt
+from typing import Optional
+import bcrypt
+import jwt
+from .main import get_moscow_time
+from datetime import timedelta
+from app.config import settings
+
 
 SECRET_KEY = "super_secret_key"
 ALGORITHM = "HS256"
 
 
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(days=7)):
+
+
+def create_access_token(data: dict, expires_minutes: int = 720) -> str:
     to_encode = data.copy()
-    expire = datetime.utcnow() + expires_delta
+
+    expire = get_moscow_time() + timedelta(minutes=expires_minutes)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm="HS256")
     return encoded_jwt
