ff86180fd0de9975d90d0aeae03baa0b9132bd43
milaha <146625448+Mrkdsjjsjs@users.noreply.github.com>
2025-04-12 14:46:25 +0300

    fix

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -5,14 +5,12 @@ from pydantic import BaseModel, EmailStr
 
 class AuthRegister(BaseModel):
     phone: str
-    password: str
     first_name: Optional[str] = None
     last_name: Optional[str] = None
 
 
 class AuthLogin(BaseModel):
     phone: str
-    password: str
     
     
 class AuthOut(BaseModel):
