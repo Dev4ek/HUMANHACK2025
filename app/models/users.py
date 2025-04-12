9353efff79069062cb7d6a9307e02d8cffefa8c3
milaha <146625448+Mrkdsjjsjs@users.noreply.github.com>
2025-04-12 16:49:13 +0300

    fix dima

--- app/models/users.py
+++ app/models/users.py
@@ -5,12 +5,12 @@ from app.base import Base
 
 
 class Users(Base):
-    __tablename__ = "users"
+    tablename = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     phone: Mapped[str] = mapped_column(String(50), unique=True, nullable=False, index=True)
     first_name: Mapped[str] = mapped_column(String(50), nullable=True)
     last_name: Mapped[str] = mapped_column(String(50), nullable=True)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now, onupdate=func.now)
